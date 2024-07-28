c04f3d547a2ab46346e74f383c504755bd7fe84b
Kim <Kimrikters@gmail.com>
2024-07-28 14:55:44 -0400

    Form started

---
+++ f1_get.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+    Hello
+    <?php echo $_GET; 
+
+// $query = "SELECT driver_id";
+// for ($x = 0; $x < 34; $x++){
+    //     $query .= ", " . $x . " * " . $points[$x];
+    // }
+    // $query .= " FROM bantable";
+    ?>
+</body>
+</html>
