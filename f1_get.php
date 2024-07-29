7eb8dfe6d0e14de257ee929373398075f4ba2f0f
Kim <Kimrikters@gmail.com>
2024-07-28 21:29:30 -0400

    started fastest lap and php

--- f1_get.php
+++ f1_get.php
@@ -2,13 +2,12 @@
 <body>
 
     Hello
-    <?php echo var_dump($_GET); 
-
-// $query = "SELECT driver_id";
-// for ($x = 0; $x < 34; $x++){
-    //     $query .= ", " . $x . " * " . $points[$x];
-    // }
-    // $query .= " FROM bantable";
+    <?php 
+    $query = "SELECT driver_id";
+    for ($x = 0; $x < 34; $x++){
+        $query .= ", " . $x . " * " . $points[$x];
+    }
+    $query .= " FROM bantable";
     ?>
 </body>
 </html>
